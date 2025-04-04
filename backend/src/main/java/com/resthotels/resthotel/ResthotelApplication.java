7dbe2946e59735087a858c947be9e37796d369e2
MatiCarrera <mcarreradev12@gmail.com>
2025-04-04 19:36:16 -0300

    commit con claves ocultas

--- backend/src/main/java/com/resthotels/resthotel/ResthotelApplication.java
+++ backend/src/main/java/com/resthotels/resthotel/ResthotelApplication.java
@@ -1,5 +1,6 @@
 package com.resthotels.resthotel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResthotelApplication {
 
 	public static void main(String[] args) {
+		// Cargar variables desde el archivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.load();
+
+		// Pasar las variables como propiedades del sistema
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID", ""));
+		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY", ""));
+
 		SpringApplication.run(ResthotelApplication.class, args);
 	}
-
 }
+
